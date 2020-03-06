112eddd13093e74830d3cf5d558db458e8c114d1
cdgaleano <galeano.daniel.c@gmail.com>
2020-03-06 02:26:51 -0300

    spinner margin

--- src/common/spinner/Spinner.tsx
+++ src/common/spinner/Spinner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './spinner.scss';
 
 const Spinner = () => (
-	<div className='center'>
+	<div className='center margin-top-150'>
 		<div className="lds-ring"><div></div><div></div><div></div><div></div></div>
 	</div>
 );
